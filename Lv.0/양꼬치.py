9e2cd4d9ecf0a935b7c183a211a4274316981e68
byungsub <nq8947@naver.com>
2022-11-25 16:04:34 +0900

    Revert "Merge branch 'main' of https://github.com/songbyungsub/Programmers"
    This reverts commit 6fac9d1e92b0678cbd132e9e1120a3cd552a9c8f, reversing
    changes made to b305cf801a28a89a803d84b2ea29f5b4008c627b.

--- Lv.0/양꼬치.py
+++
@@ -1,6 +0,0 @@
-def solution(n, k):
-    answer = 12000 *n + 2000*k
-    i = 0
-    if n >= 10:
-      i = (n // 10) * 2000
-    return answer - i
--- Lv.0/외계어 사전.py
+++
@@ -1,9 +0,0 @@
-def solution(spell, dic):
-    answer = 2
-    spell.sort()
-    for i in dic:
-        if len(spell) == len(i):
-            result = sorted(list(i))
-            if spell == result: answer = 1
-            
-    return answer
