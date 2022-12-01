f249cf535464cd340cce6934cc12ba0baa0f6b4c
byung-sub <98787306+songbyungsub@users.noreply.github.com>
2022-12-01 15:51:05 +0900

    Add files via upload

---
+++ Lv.0/숨어있는 숫자의 덧셈(2).py
@@ -0,0 +1,15 @@
+def solution(my_string):
+    answer = 0
+    temp = ''
+    for i in my_string:
+        if i.isdigit():
+            temp += i
+        else:
+            if temp:
+                answer += int(temp)
+                temp = ''
+    if temp:
+        answer += int(temp)
+    return answer
+
+solution("aAb1B2cC34oOp2")
